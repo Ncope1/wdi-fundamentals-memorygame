957a635f1e2dee13c25a1025d7f8fa092d718f4b
Ladybug <Ladybug@Natalees-MBP.fios-router.home>
2017-11-23 23:57:37 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,46 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	card: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+
+{
+	card: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+	card: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+
+{
+	card: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
+
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
     	alert("You found a match!");
   			} else {
-    		alert("Sorry, try again.");
+    			alert("Sorry, try again.");
 
   		}
 	};
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-		cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].card);
+		cardsInPlay.push(cards[cardId].card);
+			console.log(cards[cardId].cardImage)
+			console.log(cards[cardId].suit)
+
 			if (cardsInPlay.length === 2) {
 				checkForMatch();
 	}
